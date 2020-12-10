d38cdce45ceb852e886bd2d7f5847ed9efce33b2
MrSteve89 <stefan.bairac@outlook.com>
2020-12-10 18:56:14 +0200

    added header component adn styles for it

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import Home from './pages/home/Home';
 import Store from './pages/store/store';
+import Header from './components/header/header';
 import './App.css';
 
 
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Route exact path='/' component={Home}/>
       <Route exact path='/store' component={Store}/>
     </div>
